283b873b73537c3e83d2ca1fe79633628c126f55
AbramenkoPetr <106261570+AbramenkoPetr@users.noreply.github.com>
2022-09-01 19:10:31 +0300

    Add files via upload

---
+++ src/components/chat-list/chat-list.js
@@ -0,0 +1,22 @@
+import { List } from "@mui/material";
+import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Chat } from "./chat";
+import "./chat-list.css"
+
+
+export const ChatList = () => {
+  const [chatList] = useState(["room1", "room2", "room3"]);
+
+  const { roomId } = useParams();
+
+  return (
+    <List component="nav" className="common"> 
+      {chatList.map((chat) => (
+        <Link key={chat} to={`/chat/${chat}`}>
+          <Chat title={chat} selected={chat === roomId} />
+        </Link>
+      ))}
+    </List>
+  );
+};
