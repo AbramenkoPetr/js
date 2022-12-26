0988c68392775b3adb35346b5d049021fd6967fa
AbramenkoPetr <106261570+AbramenkoPetr@users.noreply.github.com>
2022-12-26 18:29:32 +0300

    Add files via upload

---
+++ зад3/src/items.js
@@ -0,0 +1,63 @@
+import image1 from "../public/images/1.jpg"
+import image2 from "../public/images/2.jpg"
+import image3 from "../public/images/3.jpg"
+import image4 from "../public/images/4.jpg"
+
+import audio1 from "../public/audio/1.mp3"
+import audio2 from "../public/audio/2.mp3"
+import audio3 from "../public/audio/3.mp3"
+
+import video1 from "../public/video/1.mp4"
+import video2 from "../public/video/2.mp4"
+
+export default [
+    {
+        resource: image1,
+        type: "image",
+        title: "Рюкзак"
+    },
+    {
+        resource: image2,
+        type: "image",
+        title: "Женский костюм"
+    },
+    {
+        resource: image3,
+        type: "image",
+        title: "Шорты"
+    },
+    {
+        resource: image4,
+        type: "image",
+        title: "Брюки"
+    },
+
+    {
+        resource: video1,
+        type: "video",
+        title: "Подарок жене"
+    },
+    {
+        resource: video2,
+        type: "video",
+        title: "Пьяный кот"
+    },
+
+
+    {
+        resource: audio1,
+        type: "audio",
+        title: "Поют птицы"
+    },
+    {
+        resource: audio2,
+        type: "audio",
+        title: "Ручей в ущелье"
+    },
+    {
+        resource: audio3,
+        type: "audio",
+        title: "Закат в лесу"
+    }
+    
+]
